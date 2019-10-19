32f87c8788de067ca8fbbcd1bdc6435bb3cd4f0c
angel-torres <alas4224@gmail.com>
2019-10-19 14:08:09 -0500

    updated Operators

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const NumberButton = (props) => {
-  console.log(props.number, "this is the number coming from props")
+  console.log(props, "this is the number coming from props")
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.number}
-    </>
+    </button>
   );
 };
 
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -27,3 +27,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
